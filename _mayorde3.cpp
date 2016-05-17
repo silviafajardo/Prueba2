26e16020c6c5ff9e8f6426228049f9e27ba7252c
silviafajardo <medusa@ucol.mx>
2016-05-17 12:41:18 -0500

    Add files via upload

---
+++ _mayorde3.cpp
@@ -0,0 +1,48 @@
+// EJERCICIO 1
+
+#include <iostream>
+using namespace std;
+
+int menor(int n1, int n2, int n3);
+
+int main(){
+     int n1, n2, n3;
+
+  cout <<"Introducir #1 Numeros";
+   cin >>n1;
+
+   cout <<"Introducir #2 Numeros";
+   cin >>n2;
+
+   cout <<"Introducir #3 Numeros";
+   cin >>n3;
+
+  int menor(int n1,int n2,int n3);
+
+
+
+  {
+      if (n1>n2) {
+
+        cout <<"EL MAYOR ES " <<n1<<endl;
+                 }
+        else
+        {
+
+
+        if(n2>n3) {
+
+        cout <<"EL MAYOR ES " <<n2<<endl;
+            }
+            else
+            {
+            if (n3>0)
+            {
+         cout <<"EL MAYOR ES " <<n3<<endl;
+            }
+
+return 0;
+}
+}
+}
+}
